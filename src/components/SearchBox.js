ccdb1aeb70659482c0fa19cc54784c3cdcfb2573
Vannak <vannaksuos@gmail.com>
2020-05-29 12:24:04 -0400

    added components

---
+++ src/components/SearchBox.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+function SearchBox () {
+    return (
+        <div className="form-row">
+        <div className="col-4">
+        <div className="form-inline my-2 my-lg-0 ml-auto">
+        <input type="text" class="form-control" placeholder="Search Employee name"/>
+        </div>
+        </div>
+        </div>
+)}
+
+export default SearchBox
