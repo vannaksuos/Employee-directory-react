333b90a1da65914f5b7716e92dcd008348ea3867
Vannak <vannaksuos@gmail.com>
2020-05-29 13:24:35 -0400

    editing components table

--- src/components/JumboTron.js
+++ src/components/JumboTron.js
@@ -6,7 +6,7 @@ function Jumbotron () {
     return(
         <div className="jumbotron jumbotron-fluid">
 <div className="container">
-    <h1 className="display-4">Employee Database</h1>
+    <h1 className="display-4">Employee Directory</h1>
     <p className="lead">Search Employee Info by name</p>
 </div>
 </div>
