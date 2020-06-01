ff48875d218d8cf4743c5ce66b94050a75ffc89f
Vannak <vannaksuos@gmail.com>
2020-06-01 11:09:58 -0400

    cleaning up table.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -6,40 +6,40 @@ import React from 'react'
 function Table () {
     return(
         <table className="table table-dark">
-  <thead>
+<thead>
     <tr>
-      <th scope="col">ID #</th>
-      <th scope="col">Full Name</th>
-      <th scope="col">Phone Number</th>
-      <th scope="col">Department</th>
-      </tr>
-  </thead>
-  <tbody>
+    <th scope="col">ID #</th>
+    <th scope="col">Full Name</th>
+    <th scope="col">Phone Number</th>
+    <th scope="col">Department</th>
+    </tr>
+</thead>
+<tbody>
     <tr>
-      <th scope="row">1</th>
-      <td></td>
-      <td></td>
-      <td></td>
+    <th scope="row">1</th>
+    <td></td>
+    <td></td>
+    <td></td>
     </tr>
     <tr>
-      <th scope="row">2</th>
-      <td></td>
-      <td></td>
-      <td></td>
+    <th scope="row">2</th>
+    <td></td>
+    <td></td>
+    <td></td>
     </tr>
     <tr>
-      <th scope="row">3</th>
-      <td></td>
-      <td></td>
-      <td></td>
+    <th scope="row">3</th>
+    <td></td>
+    <td></td>
+    <td></td>
     </tr>
     <tr>
-      <th scope="row">4</th>
-      <td></td>
-      <td></td>
-      <td></td>
+    <th scope="row">4</th>
+    <td></td>
+    <td></td>
+    <td></td>
     </tr>
-  </tbody>
+</tbody>
 </table>
     )
 }
